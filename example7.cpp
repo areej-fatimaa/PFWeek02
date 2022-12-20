b2112754b25908c8bb96caeec2db0aceb7b1e9f3
areej-fatimaa <reejayfatima123@gmail.com>
2022-12-20 12:58:18 +0500

    s

---
+++ example7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main() 
+{
+cout<<"*****************************************************" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*           CAR RENT MANAGMENT SYSTEM               *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*                                                   *" <<endl;
+cout<<"*****************************************************" <<endl;
+}
---
+++ example8.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"             ,ldk0kkkkkk0kl,..                    "<<endl;
+cout<<"         'lKKKKKKKKKkKKKKKKKKKKKl'                "<<endl;
+cout<<"       oKKKKKKKKKKKKKKKKKKKKKKKKKKKk0;            "<<endl;
+cout<<"     :KKKKKKKKkxxk0KKKKKKKKKKKKKKKKKKK0;          "<<endl;
+cout<<"    lKKKKKKKKkoc,. .,o0KKKKKKKKKKKKKKKKKk,        "<<endl;
+cout<<"  .dKKKKKKKKKKKKKKlx   ,ckKKKKKKKKKKKKKKKKKx;     "<<endl;
+cout<<"  oKKKKKKKKKKKKKKK0k0dl    .cxKKKKKKKKKKKKKK0c    "<<endl;
+cout<<" .KKKKKKKKKKKKKKKKKKkollodk0KKKKKKKKKKK:oKKKKK0.  "<<endl;
+cout<<" dKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK, "<<endl;
+cout<<"..KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK."<<endl;
+cout<<"llKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKc"<<endl;
+cout<<":.dKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK00KKKKKKKKKKKKKl"<<endl;
+cout<<":kKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk  ...';:coxKKK,"<<endl;
+cout<<".KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKx          0K0 "<<endl;
+cout<<"dKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK0:        x0. "<<endl;
+cout<<":KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKx      c.  "<<endl;
+cout<<".KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk.         "<<endl;        
+cout<<"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKlxll;        "<<endl;
+cout<<":KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK0o,'..       "<<endl;
+cout<<"oKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK...        "<<endl;
+cout<<"l.0KKKKK0xKKKKKKKKKl0KKKKKKKKKKKKKKKKKKo           "<<endl;
+cout<<"c:.clxx, dxK,:0Kko::,;0KKKKKKKKKKKKxoKc           "<<endl;
+cout<<".  '.o   .o.  .;k'    cKd'.d0KKKKKK, c            "<<endl;
+cout<<"    '     '      .      ':   dKKK.                "<<endl;
+cout<<"                             .o0ok.               "<<endl;
+cout<<"                               .. l'.             "<<endl;
+}
